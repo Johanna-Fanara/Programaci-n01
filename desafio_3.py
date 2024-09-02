2b3e1032d1aeb7a5d740b7533e6f4b1d506d4394
jfanara <124005769+Johanna-Fanara@users.noreply.github.com>
2024-09-02 10:48:14 -0300

    desafio 3 - revisado

---
+++ desafio_3.py
@@ -0,0 +1,4 @@
+"""En este desafío, los estudiantes deben desarrollar una pequeña aplicación que gestione una lista 
+de estudiantes y sus calificaciones en diferentes materias. 
+El objetivo es aplicar slicing, ordenar datos por múltiples criterios y formatear la salida usando f-strings. """
+
